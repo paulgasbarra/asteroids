b0bd05ea8b6f61c1c8c895121aebd54423686128
Paul Gasbarra <paulgasbarra@gmail.com>
2025-02-07 07:28:11 -0500

    bugfix: get display working

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from constants import *
 
 
 def main():
-    print("Starting asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
-
-if __name__ == "__main__":
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+                
+        screen.fill("black")
         pygame.screen.flip()
+
+if __name__ == "__main__":
     main()
+   
+   
