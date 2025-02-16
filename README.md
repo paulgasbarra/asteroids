0aad74200b17b3c779a093c8b43c25e94802278b
Paul Gasbarra <paulgasbarra@gmail.com>
2025-02-16 08:33:03 -0500

    feat: add readme and font

---
+++ README.md
@@ -0,0 +1,5 @@
+# Paul's Asteroids
+
+This project was begun as part of [boot.dev](https://www.boot.dev) side project. I've continued to expand it. It is for fun only.
+
+I'm using sound effects from [Adobe's Sound Library](https://www.adobe.com/products/audition/offers/adobeauditiondlcsfx.html) and a font called ["Screaming Neon"](https://www.1001fonts.com/screaming-neon-font.html#license) designed by Michael W. Moss.
--- score.py
+++ score.py
@@ -1,12 +1,13 @@
 import pygame
 from constants import ASTEROID_MIN_RADIUS, SCREEN_WIDTH, SCREEN_HEIGHT
 
+
 class Score(pygame.font.Font):
     def __init__(self, x, y):
         self.score = 0
 
     def draw(self, screen):
-        font = pygame.font.Font(None, 36)
+        font = pygame.font.Font("fonts/screaming_neon.ttf", 36)
         text = font.render(f"{self.score}", True, (255, 255, 255))
         text_rect = text.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT - 40))
         screen.blit(text, text_rect)
