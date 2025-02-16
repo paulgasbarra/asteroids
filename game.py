319d330ea2bb3562d7ae6bfb3635be20e2f42885
Paul Gasbarra <paulgasbarra@gmail.com>
2025-02-16 16:40:36 -0500

    refactor: make a game class

---
+++ game.py
@@ -0,0 +1,68 @@
+import pygame
+from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+from score import Score
+
+class Game():
+
+    def play(self):
+        
+        pygame.init()
+        pygame.font.init()
+
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        clock = pygame.time.Clock()
+
+        updatable = pygame.sprite.Group()
+        drawable = pygame.sprite.Group()
+        asteroids = pygame.sprite.Group()
+        shots = pygame.sprite.Group()
+        
+        Asteroid.containers = (asteroids, updatable, drawable)
+        AsteroidField.containers = updatable
+        asteroid_field = AsteroidField()
+
+        Shot.containers = (shots, updatable, drawable)
+    
+        Player.containers = (updatable, drawable)
+
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+        score = Score(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+        dt = 0
+
+        
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                    
+            updatable.update(dt)
+
+            for asteroid in asteroids:
+                if asteroid.did_collide_with(player):
+                    print("Game over!")
+                    return 
+            
+            for asteroid in asteroids:
+                for shot in shots:
+                    if asteroid.did_collide_with(shot):
+                        score.increase(asteroid)
+                        asteroid.split()
+                        shot.kill()
+
+            screen.fill("black")
+
+            for obj in drawable:
+                obj.draw(screen)
+
+            score.draw(screen)
+
+            pygame.display.flip()
+
+            dt = clock.tick(60) / 1000
